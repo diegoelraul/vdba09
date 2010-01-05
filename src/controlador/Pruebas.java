503e401bae10c906979be220cca30dbcfbe4b4dd
pepitogrillo37 <pepitogrillo37@023bc3f0-f9a2-11de-aabd-2bfef65f77b5>
2010-01-05 21:20:32 +0000

    guarda en la bbdd!!

--- src/controlador/Pruebas.java
+++ src/controlador/Pruebas.java
@@ -1,74 +1,75 @@
 package controlador;
 
-//import hibernate.HibernateSessionFactory;
+import hibernate.HibernateSessionFactory;
 
 //import org.hibernate.Criteria;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.Transaction;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import java.util.*;
 import modelo.compra.*;
-//import modelo.pelicula.*;
+import modelo.pelicula.*;
 //import modelo.usuario.*;
 //import modelo.reparto.*;
 
 public class Pruebas {
 	
-	//private static SessionFactory sessionFactory;
-	//@SuppressWarnings("unused")
-	//private HibernateSessionFactory hibernateSessionFactory;
 	
-	@SuppressWarnings("unchecked")
-	public static void testLecturaBD() {
+	
+	//private static SessionFactory sessionFactory;
+	private HibernateSessionFactory hibernateSessionFactory;
+	
+	public Pruebas(){}
+	
+	
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public void testLecturaBD() {
 		
+		try{
 		List resultado = null;
 		CompraDAO cdao = new CompraDAO();
 		resultado = cdao.findAll();
 		Iterator ite =((java.util.List) resultado).iterator();
 		
-		Compra c = null; //(Compra) resultado.get(0);
 		
-		/* POR FIN!!!! */
-		//System.out.println(c.getId().getPelicula());
+    	Session session = HibernateSessionFactory.getSession().getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+		
+		Compra c = null; //(Compra) resultado.get(0);
 		
 		while (ite.hasNext()){
 			c = (Compra) ite.next();
 			System.out.println("Pelicula: " + c.getId().getPelicula() + " Comprado por: " + c.getId().getUsuario());
 		}
 		
+		/* prueba de salvado. ok */
+		cdao = new CompraDAO();
+		CompraId ci = new CompraId("Eva83", "A, B, C... Manhattan", new Date(2001,11, 03));
+		Compra b = new Compra(ci);
 		
-		
-		
-		
-		/* A tomar por saco. Ahora se hace de manera distinta. */
+		cdao.save(b);
+		HibernateSessionFactory.getSession().flush();
 		/*
-		Criteria criterio = null;
+		cdao = new CompraDAO();
+		ci = new CompraId("Eva83", "Nabonga, el gorila", new Date(2001,11, 03));
+		b = new Compra(ci);
 		
-		
-		try {
-			sessionFactory = HibernateSessionFactory.getSession().getSessionFactory();
-			Session session = sessionFactory.openSession();
-			Transaction tx = session.beginTransaction();
-			CompraDAO cdao = new CompraDAO();
-			
-			resultado = cdao.findAll();
-			
-			
-			criterio = session.createCriteria(Compra.class);
-			resultado = criterio.list();
-			tx.commit();
-			
-			for (Iterator iterator =((java.util.List) resultado).iterator(); iterator.hasNext();){
-				Compra c = (Compra) iterator.next();
-				
-				//System.out.println("compra realizada por " + compra.);
-			}
-			
-			
-			
-		} catch (Exception e){}
-		
+		cdao.save(b);
 		*/
+		PeliculaDAO pdao = new PeliculaDAO();
+		
+		Pelicula p = new Pelicula("pruebas", "p", "p", "spanish",1.00, 1.00, 12, "nulo" );
+		pdao.save(p);
+		
+		
+		//super(titulo, nombreDirector, apellidosDirector, idioma, coste, precio, disponibilidad, pathImagen); 
+		
+		tx.commit();
+		HibernateSessionFactory.closeSession();
+		//session.close()
+		} catch (Exception e){
+			System.out.println(e.toString());
+		}
 	}
 
 	/**
@@ -76,7 +77,8 @@ public class Pruebas {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		testLecturaBD();
+		Pruebas p = new Pruebas();
+		p.testLecturaBD();
 	}
 
 }
