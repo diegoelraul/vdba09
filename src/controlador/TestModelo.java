d07bda4c4868fb14c5e36cb63692675bbc81694a
Arpaksad.Lyel <Arpaksad.Lyel@023bc3f0-f9a2-11de-aabd-2bfef65f77b5>
2010-01-09 20:02:01 +0000

    Primera versión del controlador para administrador, incluyendo los struts/forms y action's,

---
+++ src/controlador/TestModelo.java
@@ -0,0 +1,595 @@
+package controlador;
+
+import hibernate.HibernateSessionFactory;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Date;
+
+import modelo.*;
+import modelo.usuario.*;
+import modelo.compra.*;
+import modelo.pelicula.*;
+import modelo.reparto.*;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.MatchMode;
+
+import java.text.SimpleDateFormat;
+
+
+public class TestModelo {
+	private Session session;
+	 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			TestModelo libraryTest=new TestModelo();
+			libraryTest.setSession(HibernateSessionFactory.getSession());
+			//libraryTest.crearUsuario();
+			//libraryTest.eliminarUsuario("Karl2");
+			//libraryTest.listarUsuarios();
+			//libraryTest.getUsuario("Karl");
+			//libraryTest.crearPelicula("ABAJO EL GORILA","Swapanglish","Jardemor","Condemor",10,5.999,2.000,"vacio");
+			//libraryTest.getPelicula("Abajo el telon");
+			//libraryTest.eliminarPelicula("Abajo el gorila");
+			//libraryTest.listarPeliculas();
+			//libraryTest.listarReparto("Abajo el telon");
+			//libraryTest.getReparto("Abajo el Telon","Mario","Moreno");
+			libraryTest.listarCompra();
+			//libraryTest.createCustomer();
+			//libraryTest.createRelation();
+			//libraryTest.deleteCustomer();
+			//libraryTest.listBooks();
+			//libraryTest.getSession().close();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}
+		
+	}
+// ----------------------------
+// Gestión de datos de Usuarios
+// ----------------------------
+	public Boolean crearUsuario(String idUsuario, String password, String email,
+			String tipo, String nombre, String apellidos, String telefono,
+			Date fecAcceso){
+		System.out.println("create user");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			UsuarioDAO usuarioDAO=new UsuarioDAO();
+			usuarioDAO.save(new Usuario(idUsuario.trim(),  password,  email,
+					 tipo,  nombre,  apellidos,  telefono,
+						fecAcceso));
+			
+			 
+			//session.save(usuario);
+			tx.commit();
+			return true;
+		}catch(HibernateException e){
+			System.out.println("El usuario que quiere crear ya existe.");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public Boolean eliminarUsuario(String idUsuario){
+		System.out.println("eliminar usuario");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		
+		try{
+			UsuarioDAO usuarioDAO=new UsuarioDAO();
+			Usuario tofind=(Usuario)session.get(Usuario.class,idUsuario);
+			if(tofind==null)System.out.println("El usuario especificado no existe");
+			else{
+				usuarioDAO.delete(tofind);
+				tx.commit();
+				System.out.println("El usuario ha sido eliminado");
+				return true;
+			}
+			
+		}catch(HibernateException e){
+			System.out.println("El usuario especificado no existe");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public Usuario[] listarUsuarios(){
+		System.out.println("listar usuarios");
+		List usuarios=new ArrayList();
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			UsuarioDAO usuarioDAO=new UsuarioDAO();
+			usuarios=usuarioDAO.findAll();
+			for(Iterator iter=usuarios.iterator();iter.hasNext();){
+				System.out.println(((Usuario)iter.next()).getIdUsuario());
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			System.out.println("No se ha podido obtener info de usuarios");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return (Usuario[])usuarios.toArray(new Usuario[0]);
+	}
+	
+	public Usuario getUsuario(String usuarioId){
+		System.out.println("getUsuario");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			Usuario tofind=(Usuario)session.get(Usuario.class,usuarioId);
+			if(tofind==null)
+				System.out.println("El usuario especificado no existe");
+			else{
+				System.out.println(tofind);
+				tx.commit();
+				return tofind;
+			}
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Boolean modificarUsuario(String idUsuario, Usuario newUser){
+		Usuario old=getUsuario(idUsuario);
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			old.setIdUsuario(newUser.getIdUsuario());
+			old.setPassword(newUser.getPassword());
+			old.setFecAcceso(newUser.getFecAcceso());
+			tx.commit();
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+// ----------------------------
+// Gestión de datos de Peliculas
+// ----------------------------
+	public Boolean crearPelicula(String titulo, String nombreDirector,
+			String apellidosDirector, String idioma, double coste,
+			double precio, Integer disponibilidad, String pathImagen,
+			String anyo, String sinopsis){
+		System.out.println("crear pelicula");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			PeliculaDAO peliculaDAO=new PeliculaDAO();
+			peliculaDAO.save(new Pelicula(titulo,nombreDirector,apellidosDirector,
+					idioma,coste,precio,disponibilidad,pathImagen,anyo,sinopsis));
+			tx.commit();
+			return true;
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Boolean eliminarPelicula(String titulo){
+		System.out.println("eliminar pelicula");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			PeliculaDAO peliculaDAO=new PeliculaDAO();
+			Pelicula tofind=(Pelicula)session.get(Pelicula.class,titulo);
+			if(tofind==null)System.out.println("La pelicula especificada no existe");
+			else{
+				peliculaDAO.delete(tofind);
+				tx.commit();
+				System.out.println("La pelicula ha sido eliminada");
+				return true;
+			}
+
+		}catch(HibernateException e){
+			System.out.println("La pelicula especificada no existe");
+			rollback(tx);
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public Pelicula[] listarPeliculas(){
+		System.out.println("listar peliculas");
+		List peliculas=new ArrayList();
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			PeliculaDAO peliculaDAO=new PeliculaDAO();
+			peliculas=peliculaDAO.findAll();
+			for(Iterator iter=peliculas.iterator();iter.hasNext();){
+				Pelicula aux=(Pelicula)iter.next();
+				System.out.println(aux.getTitulo()+" "+aux.getSinopsis());
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			System.out.println("No se ha podido obtener info de peliculas");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return (Pelicula[])peliculas.toArray(new Pelicula[0]);
+	}
+
+	public Pelicula getPelicula(String titulo){
+			System.out.println("getPelicula");
+			setSession(HibernateSessionFactory.getSession());
+			Transaction tx=session.beginTransaction();
+			try{
+				Pelicula tofind=(Pelicula)session.get(Pelicula.class,titulo);
+				if(tofind==null)
+					System.out.println("La pelicula especificada no existe");
+				else{
+					System.out.println(tofind.getSinopsis());
+					tx.commit();
+					return tofind;
+				}
+			}catch(HibernateException e){
+				rollback(tx);
+				e.printStackTrace();
+			}
+			return null;
+	}
+	
+	public Boolean modificarPelicula(String titulo, Pelicula newPeli){
+		System.out.println("Se ha solicitado modificar la película: "+titulo);
+		Pelicula old=getPelicula(titulo);
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			old.setTitulo(newPeli.getTitulo());
+			old.setNombreDirector(newPeli.getNombreDirector());
+			old.setApellidosDirector(newPeli.getApellidosDirector());
+			old.setIdioma(newPeli.getIdioma());
+			old.setCoste(newPeli.getCoste());
+			old.setPrecio(newPeli.getPrecio());
+			old.setPathImagen(newPeli.getPathImagen());
+			old.setDisponibilidad(newPeli.getDisponibilidad());
+			tx.commit();
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+// ----------------------------
+// Gestión de datos de Reparto
+// ----------------------------
+	public Boolean crearReparto(String titulo,String nombre,String apellidos,String personaje){
+		System.out.println("crear reparto");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			RepartoDAO repartoDAO=new RepartoDAO();
+			// Esta parte puede eliminarse ya que la BBDD tiene configurada
+			// la relación CASCADE UPDATE y DELETE, sólo es un control extra
+			// para verificar que la película existe en BBDD.
+			Pelicula target=(Pelicula)session.get(Pelicula.class,titulo);
+			if(target==null)System.out.println("La pelicula a la que hace referencia no existe.");
+			else{
+				repartoDAO.save(new Reparto(new RepartoId(titulo,nombre,apellidos),personaje));
+				tx.commit();
+				return true;
+			}
+		}catch(HibernateException e){
+			System.out.println("El registro ya existe en BBDD.");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Boolean eliminarReparto(String titulo,String nombre,String apellidos){
+		System.out.println("eliminar reparto");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			RepartoDAO repartoDAO=new RepartoDAO();
+			RepartoId tofindId=new RepartoId(titulo,nombre,apellidos);
+			Reparto tofind=(Reparto)session.get(Reparto.class,tofindId);
+			if(tofind==null)System.out.println("El registro no existe");
+			else{
+				repartoDAO.delete(tofind);
+				tx.commit();
+				System.out.println("Se ha eliminado el registro");
+				return true;
+			}
+
+		}catch(HibernateException e){
+			System.out.println("El registro no existe");
+			rollback(tx);
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+	
+	public void eliminarReparto(Reparto reparto){
+		System.out.println("Se va a eliminar reparto");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			RepartoDAO repartoDAO=new RepartoDAO();
+			repartoDAO.delete(reparto);
+		}catch(HibernateException e){
+			System.out.println("El registro no existe");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		
+	}
+
+	public Reparto[] listarReparto(String titulo){
+		System.out.println("listar reparto de: "+titulo);
+		List reparto=new ArrayList();
+		List res=new ArrayList();
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			/*Criteria criterio=session.createCriteria(RepartoId.class).add(Expression.like("titulo",titulo,MatchMode.ANYWHERE));
+					repartoId=criterio.list();
+					System.out.println(repartoId.size());
+					reparto=criterio.list();
+					for(Iterator iter=repartoId.iterator();iter.hasNext();){
+						reparto.add((Reparto)session.get(Reparto.class,(RepartoId)iter.next()));
+					}*/
+			RepartoDAO repartoDAO=new RepartoDAO();
+			reparto=repartoDAO.findAll();
+			for(Iterator iter=reparto.iterator();iter.hasNext();){
+				Reparto aux=(Reparto)iter.next();
+				System.out.println(aux.getId().getTitulo());
+				if(aux.getId().getTitulo().trim().toUpperCase().equals(titulo.trim().toUpperCase())){
+					res.add(aux);
+					System.out.println(aux.getPersonaje());
+				}
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			System.out.println("No se ha podido obtener info de reparto");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return (Reparto[])res.toArray(new Reparto[0]);
+
+	}
+
+	public Reparto getReparto(String titulo,String nombre,String apellidos){
+		System.out.println("getReparto");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			Reparto tofind=(Reparto)session.get(Reparto.class,new RepartoId(titulo.trim(),nombre.trim(),apellidos.trim()));
+			if(tofind==null)
+				System.out.println("El registro especificado no existe");
+			else{
+				System.out.println(tofind.getPersonaje());
+				tx.commit();
+				return tofind;
+			}
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+// ----------------------------
+// Gestión de datos de Compra
+// ----------------------------
+	public Boolean crearCompra(String usuario,String pelicula,Date fecha,Date hora,String formapago){
+		System.out.println("crear Compra");
+		setSession(HibernateSessionFactory.getSession());
+		Transaction tx=session.beginTransaction();
+		try{
+			CompraDAO compraDAO=new CompraDAO();
+			CompraId compraID=new CompraId(usuario,pelicula,fecha,hora);
+			compraDAO.save(new Compra(compraID,formapago));
+			tx.commit();
+			return true;
+		
+		}catch(HibernateException e){
+			System.out.println("El registro ya existe en BBDD.");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Compra[] listarCompra(){
+		System.out.println("listar compras");
+		List compra=new ArrayList();
+		List res=new ArrayList();
+		Transaction tx=session.beginTransaction();
+		try{
+			CompraDAO compraDAO=new CompraDAO();
+			compra=compraDAO.findAll();
+			for(Iterator iter=compra.iterator();iter.hasNext();){
+				res.add((Compra)session.get(Compra.class,(CompraId)iter.next()));
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			System.out.println("No se ha podido obtener info de compra");
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return (Compra[])res.toArray(new Compra[0]);
+	}
+/*
+	public Compra getCompra(String titulo,String nombre,String apellidos){
+		System.out.println("getCompra");
+		Transaction tx=session.beginTransaction();
+		try{
+			Compra tofind=(Compra)session.get(Compra.class,new CompraId(titulo.trim(),nombre.trim(),apellidos.trim()));
+			if(tofind==null)
+				System.out.println("El registro especificado no existe");
+			else{
+				System.out.println(tofind.getPersonaje());
+				tx.commit();
+				return tofind;
+			}
+		}catch(HibernateException e){
+			rollback(tx);
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+*/
+// --------------------------------------------------
+// Realiza un Rollback de Transaction de forma segura
+// --------------------------------------------------
+	public void rollback(Transaction t){
+		if(t!=null){
+			try{
+				t.rollback();
+			}catch(Exception e){
+				System.out.println("No se ha podido realizar tx.rollback()");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+/*	public void createCustomer(){
+		System.out.println("create customer");
+		try{
+			Transaction tx=session.beginTransaction();
+			Customer customer=new Customer();
+			customer.setLastname("Fitz");
+			customer.setName("Jhon");
+			customer.setAge(new Integer(25));
+			session.save(customer);
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void createRelation(){
+		System.out.println("create relation");
+		try{
+			Transaction tx=session.beginTransaction();
+			Customer customer=new Customer();
+			customer.setLastname("Schmidt");
+			customer.setName("Jim");
+			customer.setAge(new Integer(25));
+			session.save(customer);
+			Book book=new Book();
+			book.setAuthor("Gerhard Petter");
+			book.setTitle("Gerhards biography");
+			book.setAvailable(Boolean.TRUE);
+			session.save(book);
+			Book book2=new Book();
+			book2.setAuthor("Karl May");
+			book2.setTitle("Wildes Kurdistan");
+			book2.setAvailable(Boolean.TRUE);
+			session.save(book2);
+			session.flush();
+			book.setCustomer(customer);
+			book2.setCustomer(customer);
+			tx.commit();
+			session.refresh(customer);
+			tx=session.beginTransaction();
+			if(customer.getBooks()!=null){
+				System.out.println("list books");
+				for(Iterator iter=customer.getBooks().iterator();iter.hasNext();){
+					Book element=(Book)iter.next();
+					System.out.println("customer: "+element.getCustomer());
+					System.out.println("customer is now:"+element.getCustomer());
+				}
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void deleteCustomer(){
+		System.out.println("delete customer");
+		try{
+			Transaction tx=session.beginTransaction();
+			Customer customer=new Customer();
+			customer.setLastname("Wumski");
+			customer.setName("Gerhard");
+			customer.setAge(new Integer(25));
+			session.save(customer);
+			Book book=new Book();
+			book.setAuthor("Tim Tom");
+			book.setTitle("My new biography");
+			book.setAvailable(Boolean.TRUE);
+			session.save(book);
+			book.setCustomer(customer);
+			tx.commit();
+			tx=session.beginTransaction();
+			session.refresh(customer);
+			session.delete(customer);
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+			
+		}
+	}
+	
+	private void listBooks(){
+		System.out.println("list customers");
+		Criteria query;
+		Transaction tx;
+		List customers=new ArrayList();
+		List books=new ArrayList();
+		try{
+			tx=session.beginTransaction();
+			query=session.createCriteria(Customer.class);
+			customers=query.list();
+			for(Iterator iter=customers.iterator();iter.hasNext();){
+				System.out.println((Customer)iter.next());
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}
+		System.out.println("list books");
+		try{
+			tx=session.beginTransaction();
+			query=session.createCriteria(Book.class);
+			books=query.list();
+			for(Iterator iter=books.iterator();iter.hasNext();){
+				System.out.println((Book)iter.next());
+			}
+			tx.commit();
+		}catch(HibernateException e){
+			e.printStackTrace();
+		}
+	}
+*/	
+	public Session getSession(){
+		return session;
+	}
+	
+	public void setSession(Session s){
+		this.session=s;
+	}
+	
+}
---
+++ src/hibernate/struts/action/EditarPeliculaAction.java
@@ -0,0 +1,172 @@
+package hibernate.struts.action;
+
+import hibernate.struts.form.EditarPeliculaForm;
+
+import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import org.apache.struts.action.Action;
+import controlador.TestModelo;
+import java.util.Date;
+import modelo.pelicula.Pelicula;
+
+/** 
+ * MyEclipse Struts
+ * Creation date: 01-07-2010
+ * 
+ * XDoclet definition:
+ * @struts.action path="/editarUsuario" name="editarUsuarioForm" parameter="do" scope="request" validate="true"
+ * @struts.action-forward name="eliminarUsuario" path="/jsp/eliminarUsuario.jsp"
+ * @struts.action-forward name="modificarUsuario" path="/jsp/modificarUsuario.jsp"
+ * @struts.action-forward name="nuevoUsuario" path="/jsp/nuevoUsuario.jsp"
+ */
+public class EditarPeliculaAction extends DispatchAction {
+	
+	
+	public ActionForward eliminarPelicula(
+			             ActionMapping mapping,
+			             ActionForm form,
+			             HttpServletRequest request,
+			             HttpServletResponse response){
+		System.out.print("eliminar pelicula: ");
+		EditarPeliculaForm editarPeliculaForm=(EditarPeliculaForm)form;
+		TestModelo admin=new TestModelo();
+		editarPeliculaForm.setPelicula(admin.getPelicula(request.getParameter("titulo")));
+		return mapping.findForward("eliminarPelicula");
+	}
+	
+	public ActionForward elimPeliculaConf(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		TestModelo admin=new TestModelo();
+		admin.eliminarPelicula(request.getParameter("titulo"));
+		System.out.println("Eliminada la película");
+		return mapping.findForward("elimPeliculaConf");
+	}
+	
+	public ActionForward verPelicula(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		EditarPeliculaForm editarPeliculaForm=(EditarPeliculaForm)form;
+		TestModelo admin=new TestModelo();
+		System.out.println("Se ha solicitado ver detalle de película:"+request.getParameter("titulo"));
+		System.out.println(editarPeliculaForm);
+		editarPeliculaForm.setPelicula(admin.getPelicula(request.getParameter("titulo")));
+		editarPeliculaForm.setReparto(admin.listarReparto(request.getParameter("titulo")));
+//		response.setAttribute("pel",admin.getPelicula(request.getParameter("titulo")));
+		return mapping.findForward("verPelicula");
+	}
+	
+	public ActionForward elimReparto(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		EditarPeliculaForm editarPeliculaForm=(EditarPeliculaForm)form;
+		TestModelo admin=new TestModelo();
+		System.out.println("Se ha solicitado eliminar de la peli:"+editarPeliculaForm.getPelicula().getTitulo());
+		System.out.println("El personaje:"+request.getParameter("personaje"));
+		
+		//admin.eliminarReparto(request.getParameter("id"));)
+		return mapping.findForward("elimReparto");
+	}
+	
+	public ActionForward nuevaPelicula(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		System.out.println("Se ha solicitado crear una peli");
+		return mapping.findForward("nuevaPelicula");
+	}
+	
+	public ActionForward modifPelicula(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		System.out.println("Se ha solicitado modif una peli");
+		
+		EditarPeliculaForm editarPeliculaForm=(EditarPeliculaForm)form;
+		System.out.println("Form apellidosDirector:"+editarPeliculaForm.getApellidosDirector());
+		String titulo=request.getParameter("titulo");
+		System.out.println("titulo:"+titulo);
+		String apellidosDirector=request.getParameter("apellidosDirector");
+		System.out.println("apellidosDirector: "+apellidosDirector);
+		System.out.println(request.getParameter("coste"));
+		/*Double coste=new Double(request.getParameter("coste"));
+		Integer disponibilidad=new Integer(request.getParameter("disponibilidad"));
+		String idioma=request.getParameter("idioma");
+		String nombreDirector=request.getParameter("nombreDirector");
+		String pathImagen=request.getParameter("pathImagen");
+		Double precio=new Double(request.getParameter("precio"));
+		Pelicula newPeli=new Pelicula(titulo,idioma,
+				apellidosDirector,nombreDirector,
+				precio,coste,disponibilidad,pathImagen);*/
+		TestModelo admin=new TestModelo();
+		//admin.modificarPelicula(request.getParameter("titulo"),newPeli);*/
+		editarPeliculaForm.setPelicula(admin.getPelicula(request.getParameter("titulo")));
+		return mapping.findForward("modifPelicula");
+	}
+	
+	public ActionForward modifPeliculaSave(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		System.out.println("Se ha solicitado guardar la modif de una peli");
+		EditarPeliculaForm editarPeliculaForm=(EditarPeliculaForm)form;
+		System.out.println("Form apellidosDirector:"+editarPeliculaForm.getApellidosDirector());
+		String titulo=request.getParameter("titulo");
+		System.out.println("titulo:"+titulo);
+		String apellidosDirector=request.getParameter("apellidosDirector");
+		System.out.println("apellidosDirector: "+apellidosDirector);
+		System.out.println(request.getParameter("coste"));
+		Double coste=new Double(request.getParameter("coste"));
+		Integer disponibilidad=new Integer(request.getParameter("disponibilidad"));
+		String idioma=request.getParameter("idioma");
+		String nombreDirector=request.getParameter("nombreDirector");
+		String pathImagen=request.getParameter("pathImagen");
+		Double precio=new Double(request.getParameter("precio"));
+		String anyo=request.getParameter("anyo");
+		String sinopsis=request.getParameter("sinopsis");
+		Pelicula newPeli=new Pelicula(titulo,nombreDirector,apellidosDirector,
+				idioma,coste,precio,disponibilidad,pathImagen,anyo,sinopsis);
+		TestModelo admin=new TestModelo();
+		admin.modificarPelicula(titulo,newPeli);
+		return mapping.findForward("modifPeliculaSave");
+	}
+	
+	public ActionForward nuevaPeliculaSave(
+            ActionMapping mapping,
+            ActionForm form,
+            HttpServletRequest request,
+            HttpServletResponse response){
+		String titulo=request.getParameter("titulo");
+		String apellidosDirector=request.getParameter("apellidosDirector");
+		Double coste=new Double(request.getParameter("coste"));
+		Integer disponibilidad=new Integer(request.getParameter("disponibilidad"));
+		String idioma=request.getParameter("idioma");
+		String nombreDirector=request.getParameter("nombreDirector");
+		String pathImagen=request.getParameter("pathImagen");
+		Double precio=new Double(request.getParameter("coste"));
+		String anyo=request.getParameter("anyo");
+		String sinopsis=request.getParameter("sinopsis");
+		
+		System.out.println(titulo+" "+apellidosDirector+" "+nombreDirector+" "+coste+" "+disponibilidad);
+		
+		Date fecAcceso=new Date();
+		TestModelo admin=new TestModelo();
+		admin.crearPelicula(titulo,nombreDirector,apellidosDirector,
+				idioma,coste,precio,disponibilidad,pathImagen,anyo,sinopsis);
+		System.out.println("La pelicula se ha creado");
+		return mapping.findForward("nuevaPeliculaSave");
+	}
+}
